2a6ac181d7f1e01fffcdd820b54a8a67d97b77ee
Kishore1999 <62424094+Kishore1999@users.noreply.github.com>
2020-03-31 17:31:23 +0530

    commit a changes

--- studentdetails.swift
+++ studentdetails.swift
@@ -7,6 +7,9 @@ struct StudentDetails {
    private var maths : Int 
    private var totalMark : Int 
    private var percentage : Int
+   static var failArray = [String]()
+   static var  passArray = [String]()
+   static var passStudentMarks = [Int]()
    init(studentName : String,studentId : String, physics : Int, chemistry : Int, maths : Int, totalMark : Int, percentage : Int) {
         self.studentName = studentName
         self.studentId = studentId
@@ -16,18 +19,25 @@ struct StudentDetails {
         self.totalMark = totalMark
         self.percentage = percentage
         if (physics >= 50 && chemistry >= 50 && maths >= 50) {
-            passArray.append("name: " + studentName + "," + "studentId: " +  studentId + "," + "totalMark: " + "\(totalMark)")
-            passStudentMarks.append(totalMark)
+            StudentDetails.passArray.append("name: " + studentName + "," + "studentId: " +  studentId + "," + "totalMark: " + "\(totalMark)")
+            StudentDetails.passStudentMarks.append(totalMark)
         }
         else {
-            failArray.append("name: " + studentName + "," + "studentId: " +  studentId + "," + "totalMark: " + "\(totalMark)")
+            StudentDetails.failArray.append("name: " + studentName + "," + "studentId: " +  studentId + "," + "totalMark: " + "\(totalMark)")
         }
     }
-    
+    func ranklist()  {
+         let rankList = StudentDetails.passStudentMarks.sorted(by: > )
+         for rank in 0..<rankList.count {
+             print("Rank \(rank + 1) total is \(rankList[rank])")
+         }
+        print(rankList)
+    }
 }
-var failArray : [String] = [], passArray : [String] = [], passStudentMarks : [Int] = []
-var studentDetails = ["kishore,101,kishore@gmail.com,{physics:100,maths:60,chemistry:60}","vasu,102,vasu@skct.com,{physics:100,maths:90,chemistry:90}"]
+var count = 0
+var studentDetails = ["kishore,101,kishore@gmail.com,{physics:100,maths:100,chemistry:60}","vasu,102,vasu@skct.com,{physics:100,maths:90,chemistry:90}","mithun,103,mithun@gmail.com,{physics:50,maths:100,chemistry:60}"]
 for indiviual  in studentDetails {
+    count += 1
     let separatedByBraces = indiviual.replacingOccurrences(of :"}" ,with: ",")
     let separatedStudentDetails =  separatedByBraces.components(separatedBy: ",")
     let separatedByPhysics = separatedByBraces.components(separatedBy: "physics:")
@@ -43,22 +53,18 @@ for indiviual  in studentDetails {
         let beforeAt = email.components(separatedBy: "@")
         let afterDot = email.components(separatedBy: ".")
         if beforeAt[0] != "" && afterDot[1] != "" {
-            var student1 = StudentDetails(studentName:separatedStudentDetails[0], studentId:separatedStudentDetails[1], physics:Int(physicsMark[0]) ?? 0, chemistry:Int(chemistryMark[0]) ?? 0, maths:Int(mathsMark[0]) ?? 0, totalMark:totalMark, percentage: percentage )
+            var studentDetailsObject = StudentDetails(studentName:separatedStudentDetails[0], studentId:separatedStudentDetails[1], physics:Int(physicsMark[0]) ?? 0, chemistry:Int(chemistryMark[0]) ?? 0, maths:Int(mathsMark[0]) ?? 0, totalMark:totalMark, percentage: percentage )
+            if count == studentDetails.count {
+                studentDetailsObject.ranklist()
+            }
         }
+        else {
+               print("Wrong Email Format")
+           }
     }
     else {
         print("Wrong Email Format")
     }
-}
-func calculateRankList() {
-let sortedList = passStudentMarks.sorted(by: > )
-for rank in 0..<sortedList.count {
-    print("Rank \(rank + 1) total is \(sortedList[rank])")
     
 }
-}
-print(passArray)
-calculateRankList()
-
-
 
