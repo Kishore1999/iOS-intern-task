272e38635fca47569d8bef732147c82995982f09
Kishore1999 <62424094+Kishore1999@users.noreply.github.com>
2020-04-06 16:34:21 +0530

    commit changes

--- zombie.swift
+++ zombie.swift
@@ -1,31 +1,28 @@
 import Foundation
-
-//print("Hello World")
 let numberOfZombies: Int = 10
-var numberOfBullet: Int = 5
+var numberOfBullet: Int = 6
 var zombiesDistance: [Int] = ([1,6,6,8,5,8,6,7,10,9]).sorted(by: <)
 var shootCount: Int = 0
-var count: Int = 0
+var killedCount: Int = 0
 for var zombiesDistanceIndex in 0..<zombiesDistance.count  {
     if zombiesDistance[zombiesDistanceIndex] == 0 {
         print("YOU KILLED BY ZOMBIE!!!")
         break
     }
-    else if zombiesDistance[zombiesDistanceIndex] <= 5 && numberOfBullet != 0  {
-        count += 1
-        if(count == 2) {
+    else if zombiesDistance[zombiesDistanceIndex] < 5 && numberOfBullet != 0  {
+        killedCount += 1
+        numberOfBullet = numberOfBullet - 1
+        if(killedCount == 2) {
              numberOfBullet = numberOfBullet + 1
-             count = 0
+             killedCount = 0
         }
-        else {
-            numberOfBullet = numberOfBullet - 1
-        }
+        
         zombiesDistance[zombiesDistanceIndex] = -1
         shootCount += 1
-        print("*****ZOMBIE KILLED*******")
+        print("*******ZOMBIE KILLED*******")
     }
     else if numberOfBullet == 0 {
-        print("NO MORE BULLETS SO, ****GAME OVER!!!******")
+        print("NO MORE BULLETS SO, *******GAME OVER!!!*******")
         break
     }
     else {
