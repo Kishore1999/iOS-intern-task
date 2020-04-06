9c04729d55d1258bb08bbb8df4984cec10e55ef7
Kishore1999 <62424094+Kishore1999@users.noreply.github.com>
2020-04-06 20:04:21 +0530

    Question

--- zombie.swift
+++ zombie.swift
@@ -1,16 +1,24 @@
-/*given inputs = number of bullets, number of zombies, distance between player and zombies in array
+/* It's like a "shooting game".We need to kill all the zombies.In a array contains distances between zombies and players.It reduces every seconds by 1meter.shoot range also with in 5meter.
+for every two shoots we get one extra bullet.Task is to kill all Zombies with in a given bullets.After that printing killing scores.If distance between zombies and players reaches zero means 
+player died and game is over.
+
 rules:
 1.if distance is greater then 5 means we cannot shoot zombies
 2.payers get +1 bullet back after  killed 2 zombies
 3.every seconds zombie distance reduced to -1
 4. per seconds one shoot are allowed
 5.if distance is 0 means game over .player killed by zombie or else player win the game
+
+inputs :
+       number of bullets, number of zombies, distance between player and zombies in array like = [2,3,4,5]
+output:
+       Final score
 */
 
 import Foundation
-let numberOfZombies: Int = 2
+let numberOfZombies: Int = 5
 var numberOfBullet: Int = 6
-var zombiesDistance: [Int] = ([1,10]).sorted(by: <)
+var zombiesDistance: [Int] = ([1,2,6,8,4]).sorted(by: <)
 var shootCount: Int = 0
 var killedCount: Int = 0 , zombiesDistanceIndex: Int = 0
 while zombiesDistanceIndex < zombiesDistance.count  {
