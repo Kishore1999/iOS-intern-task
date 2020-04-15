a8a82f90e2aada4a242423beb5c0e6613334a676
Kishore1999 <62424094+Kishore1999@users.noreply.github.com>
2020-04-15 13:07:15 +0530

    Update eb bill.swift

--- eb bill.swift
+++ eb bill.swift
@@ -1,15 +1,21 @@
-/*Problem Statement : Need to calculate Electricity bill for every two months based on below tariffs.  
+/*
+Problem Statement : Need to calculate Electricity bill for every two months based on below tariffs.  
+
 For domestic
 For first 100 units Rs 0/unit
-For next 100 units Rs 1.50/unit
-For next 300 units Rs 4.60/unit
-For unit above 500  Rs 6.60/unit
-Fixed charges for two months Rs.50/service
+For next 100 units Rs 1.50/unit, Fixed sevice cost Rs 20
+For next 300 units Rs 3.00/unit, Fixed sevice cost Rs 30
+For unit above 500  Rs 6.60/unit , Fixed sevice cost Rs 50
+
 For Commercial
 For first 100 units Rs 5.00/unit
 Consumption above 100 units bi monthly 8.05/unit
 Fixed charges for two months Rs.290/service
-Get random numbers as daily consumable units. For domestic random number generation should be in between 1 - 10. For commercial 10 - 100*/
+
+Get random numbers as daily consumable units. 
+For domestic random number generation should be in between 1 - 10. 
+For commercial 10 - 100
+*/
 
 import Foundation
 func domestic(totalTwoMonthReading: Float) -> Float {
@@ -28,9 +34,30 @@ func domestic(totalTwoMonthReading: Float) -> Float {
     }
     
 }
-var readingUnitsArray: [Int] = []
-for eachDay in 0..<60 {
-    readingUnitsArray.append(Int.random(in:1...10))
+func commercial(totalTwoMonthReading: Float) -> Float {
+    var withOutAddingService: Float = 0
+    var serviceAmount: Float = 290 
+    if totalTwoMonthReading <= 100 {
+        withOutAddingService = Float(totalTwoMonthReading * 5)
+    }
+    else {
+        withOutAddingService = Float(100 * 5) + Float((totalTwoMonthReading - 100) * 8.05)
+    }
+    return (withOutAddingService + serviceAmount)
 }
-print(readingUnitsArray.reduce(0,+))
-print(domestic(totalTwoMonthReading:Float(readingUnitsArray.reduce(0,+))))
+var type = "commercial"
+var readingUnitsArray: [Int] = []
+if type == "domestic" {
+    for eachDay in 0..<60 {
+        readingUnitsArray.append(Int.random(in:1...10))
+    }
+    print("Total Unit: \(readingUnitsArray.reduce(0,+))")
+    print("Cost : \(domestic(totalTwoMonthReading:Float(readingUnitsArray.reduce(0,+))))")
+}
+else {
+    for eachDay in 0..<60 {
+        readingUnitsArray.append(Int.random(in:10...100))
+    }
+    print("Total Unit: \(readingUnitsArray.reduce(0,+))")
+    print("Cost : \(commercial(totalTwoMonthReading:Float(readingUnitsArray.reduce(0,+))))")
+    }
