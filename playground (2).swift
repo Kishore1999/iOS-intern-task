e90bd83a279176eb7916527b40ba6533e34d70c3
Kishore1999 <62424094+Kishore1999@users.noreply.github.com>
2020-05-05 19:56:09 +0530

    Bubble Sort

---
+++ playground (2).swift
@@ -0,0 +1,15 @@
+import Foundation
+
+var array = [2,6,98,60,72]
+var temporaryVariable = 0
+for outerIndex in 0..<array.count - 1 {
+    for innerIndex in 0..<array.count - outerIndex - 1  {
+        if array[innerIndex] > array[innerIndex + 1] {
+            temporaryVariable = array[innerIndex]
+            array[innerIndex] = array[innerIndex + 1]
+            array[innerIndex + 1] = temporaryVariable
+        }
+    }
+    
+}
+print(array)
